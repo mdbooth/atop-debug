0e76d4c634f03b63d24925f0d64bd981515f0fd4
Matthew Booth <mbooth@redhat.com>
2022-09-23 17:09:47 +0100

    Add gather-logs.sh

---
+++ gather-logs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+nodes=$(oc -n atop-debug get pod -l app=atop-debug -o json | \
+        jq -re '.items[] | .spec.nodeName')
+
+for node in ${nodes}; do
+    mkdir "${node}"
+    oc debug node/"${node}" -- tar -C /host/var/tmp/atop-debug -cf - . | \
+        tar -C "${node}" -xvf -
+done
