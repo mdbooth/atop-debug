d7de995759c67cc6f347b4051bf8ba014f28ce76
Matthew Booth <mbooth@redhat.com>
2022-09-27 15:27:18 +0100

    Use ns/atop-debug in gather-logs.sh

--- gather-logs.sh
+++ gather-logs.sh
@@ -5,6 +5,6 @@ nodes=$(oc -n atop-debug get pod -l app=atop-debug -o json | \
 
 for node in ${nodes}; do
     mkdir "${node}"
-    oc debug node/"${node}" -- tar -C /host/var/tmp/atop-debug -cf - . | \
+    oc -n atop-debug debug node/"${node}" -- tar -C /host/var/tmp/atop-debug -cf - . | \
         tar -C "${node}" -xvf -
 done
